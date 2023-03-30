4bc4ad96b6c5a053c20cac7904bf9a5cd32e6879
Cooper <Cooperransom08@gmail.com>
2023-03-30 00:44:28 -0400

    more imports from pycharm

--- 2PT-source/2PT.py
+++ 2PT-source/2PT.py
@@ -2,7 +2,8 @@
 # project or using the installer to install a modified version of this program for easy to use console app
 import random
 import string
-import hashlib
+import requests
+import time
 
 # Variables
 x32_keylength = 24
@@ -53,6 +54,56 @@ class x64key:
     return key
 
 
+class hexkey():
+
+  def rawkey():
+    threshold = random.randint(7, 16)
+    buff = random.randint(1000, 5000)
+    length = random.randint(16, 20)
+    lengthlist = [3, 4, 6]
+    if random.choice([True, False]):
+      for item in lengthlist:
+        int(random.choice(lengthlist) - 1)
+    else:
+      pass
+    iterable = 0
+    alphabet = string.digits + string.ascii_uppercase + string.digits
+    baseList = '.'.join(random.choice(alphabet) for i in range(buff)).split('.')
+
+    while iterable != threshold:
+      foo = baseList + list(random.choice(alphabet) for i in range(buff))
+      for item in range(length):
+        random.shuffle(foo)
+      iterable += 1
+
+      key = str('0' + 'x' + ''.join(
+        random.choice(foo) for iterable in range(random.choice(lengthlist))))
+
+    return key
+
+
+class wordkey():
+
+  def rawkey():
+    buff = random.randint(1000, 5000)
+    wordamount = random.randint(1, 3)
+    foo = []
+    wordlist = []
+    word_site = "https://www.mit.edu/~ecprice/wordlist.10000"
+    response = requests.get(word_site)
+    baseList = response.content.splitlines()
+
+    for i in range(wordamount):
+      foo.append(random.choice(baseList).decode('utf-8'))
+    for item in foo:
+      item = item.capitalize()
+      wordlist.append(item)
+
+    key = ''.join(wordlist) + str(random.randint(150, 999) - 75)
+
+    return key
+
+
 def split_key(key):
   keylist = list(key)
   returnkey = []
@@ -69,6 +120,14 @@ def createkey(type):
     return split_key(x32key.rawkey())
   elif type == 'x64':
     return split_key(x64key.rawkey())
+  elif type == 'hex':
+    return hexkey.rawkey()
+  elif type == 'word':
+    return wordkey.rawkey()
   else:
     return 'ERROR: Invalid key type'
 
+
+while True:
+  time.sleep(0.7)
+  print(wordkey.rawkey())
