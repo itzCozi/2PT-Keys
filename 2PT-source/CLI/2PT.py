a01a0a8118ad6992447a47c5b3490b573483c2e8
Cooper <Cooperransom08@gmail.com>
2023-03-30 17:13:20 -0400

    create website

--- 2PT-source/CLI/2PT.py
+++
@@ -1,2 +0,0 @@
-# 2PT is a framework for very secure cryptographic keys people can use this program by draging the file into there
-# project or using the installer to install a modified version of this program for easy to use console app
