80f2007178036dfe453a498274bbf0098be86a63
Cooper <Cooperransom08@gmail.com>
2023-03-31 13:10:24 -0400

    Been working in IDE jus moved the code over

--- 2PT-source/CLI/2PT.py
+++ 2PT-source/CLI/2PT.py
@@ -1,140 +1,274 @@
 # Gotta make 2PT console app. Links: https://github.com/itzCozi/Terminal-Bypass-Game (Also check if scoop is installed and if not, install it)
-import random
-import string
-import requests
+try:
+  import random
+  import string
+  import requests
+  import os, sys
+  import hashlib
+  import subprocess
+except:
+  raise ImportError(
+    'Please install the required packages: hashlib, subprocess, request and random'
+  )
 
 # Variables
 x32_keylength = 24
 x64_keylength = 32
+debug = True
 
 
-class x32key:
+class _2PT():
+  main_Dir = str('C:/Users/' + os.getlogin() + '/2PT-Keys')
+  scoop_Dir = str('C:/Users/' + os.getlogin() + '/scoop')
+  scoopApp_Dir = str(scoop_Dir + '/apps/2PT-Console')
+  scoopShim_File = str(scoop_Dir + '/shims/2PT.cmd')
+  powershell = str('C:\Windows\System32\powershell.exe')
+  scoopApp_File = str(scoop_Dir + 'apps/2PT-Console/2PT-console.py')
 
-  @staticmethod
-  def x32_rawkey():
-    threshold = random.randint(7, 16)
-    buff = random.randint(500, 1000)
-    length = random.randint(8, 20)
-    iterable = 0
-    alphabet = string.digits + string.ascii_letters + string.digits
-    baseList = '.'.join(random.choice(alphabet) for i in range(buff)).split('.')
+  def autoUpdate():
+    # Compares the hashes of the main file and the file on the website. And if they are not the same replace them
+    webFile = _2PT.utility.hashFileURL(
+      'https://itzcozi.github.io/SafeGuard/data/safeguard-files/SafeGuard-Python.py'
+    )
+    scoop_Dir = str('C:/Users/' + os.getlogin() + '/scoop')
+    localFile = _2PT.utility.hashFileLOCAL(_2PT.scoopApp_File)
 
-    while iterable != threshold:
-      foo = baseList + list(random.choice(alphabet) for i in range(buff))
-      for item in range(length):
-        random.shuffle(foo)
-      iterable += 1
+    if webFile != localFile:
+      if debug:
+        print("Program files !OUTDATED!")
+        print("Updating Program Files...")
 
-      key = ''.join(random.choice(foo) for iterable in range(x32_keylength))
+      # Update file
+      with open(scoop_Dir + 'apps/2PT-Console/2PT-console.py', "w") as f:
+        f.truncate(0)
+        f.write(
+          requests.get(
+            'https://itzcozi.github.io/2PT-Keys/data/2PT-console.py').text)
+        f.close()
 
-    return str(key)
+      if debug:
+        print("Program Successfully Updated!")
 
+  def setup():
+    scoopinstalled = False
 
-class x64key:
-
-  @staticmethod
-  def x64_rawkey():
-    threshold = random.randint(7, 16)
-    buff = random.randint(1000, 5000)
-    length = random.randint(16, 20)
-    iterable = 0
-    alphabet = string.ascii_letters + string.digits
-    baseList = '.'.join(random.choice(alphabet) for i in range(buff)).split('.')
-
-    while iterable != threshold:
-      foo = baseList + list(random.choice(alphabet) for i in range(buff))
-      for item in range(length):
-        random.shuffle(foo)
-      iterable += 1
-
-      key = ''.join(random.choice(foo) for iterable in range(x64_keylength))
-
-    return str(key)
-
-
-class hexkey():
-
-  @staticmethod
-  def hex_rawkey():
-    threshold = random.randint(7, 16)
-    buff = random.randint(1000, 5000)
-    length = random.randint(16, 20)
-    lengthlist = [3, 4, 6]
-    if random.choice([True, False]):
-      for item in lengthlist:
-        int(random.choice(lengthlist) - 1)
+    if os.path.exists(_2PT.scoop_Dir):
+      scoopinstalled = True
     else:
-      pass
-    iterable = 0
-    alphabet = string.digits + string.ascii_uppercase + string.digits
-    baseList = '.'.join(random.choice(alphabet) for i in range(buff)).split('.')
+      # NEEDS TESTING ON PC : https://www.makeuseof.com/windows-install-scoop/?newsletter_popup=1
+      subprocess.call(_2PT.powershell + 'iwr -useb get.scoop.sh | iex, shell=False')
 
-    while iterable != threshold:
-      foo = baseList + list(random.choice(alphabet) for i in range(buff))
-      for item in range(length):
-        random.shuffle(foo)
-      iterable += 1
+    os.mkdir(_2PT.main_Dir)
+    os.mkdir(_2PT.scoopApp_Dir)
 
-      key = str('0' + 'x' + ''.join(
-        random.choice(foo) for iterable in range(random.choice(lengthlist))))
+    if not os.path.exists(_2PT.scoopShim_File):
+      with open(_2PT.scoopShim_File, 'r+') as file:
+        file.write('@' + _2PT.scoopApp_File + ' %*')
+      if debug:
+        print("Program file [" + _2PT.scoopShim_File + "] !MISSING!")
 
-    return str(key)
+    if not os.path.exists(_2PT.scoopApp_File):
+      _2PT.utility.install("https://itzcozi.github.io/2PT-Keys/data/2PT-console.py",_2PT.scoopApp_File,"2PT",".cmd")
+      if debug:
+        print("Program file [" + _2PT.scoopApp_File + "] !MISSING!")
 
+  class utility():
 
-class wordkey():
+    def hashFileURL(url):
+      newFile = str('C:/Users/' + os.getlogin() + '/2PT-Keys/newfile')
 
-  @staticmethod
-  def word_rawkey():
-    wordamount = random.randint(1, 3)
-    foo = []
-    shortlist = []
-    wordlist = []
-    word_site = "https://www.mit.edu/~ecprice/wordlist.10000"
-    response = requests.get(word_site)
-    baselist = response.content.splitlines()
+      with open(newFile, "w") as f:
+        f.write(requests.get(url).text)
+        f.close()
 
-    for word in baselist:
-      if len(word) <= random.randint(6, 12):
-        shortlist.append(word)
+      BUF_SIZE = os.path.getsize(newFile)
+      sha256 = hashlib.sha256()
+      with open(newFile, 'rb') as f:
+        while True:
+          data = f.read(BUF_SIZE)
+          if not data:
+            break
+
+      sha256.update(data)
+
+      f.close()
+      os.remove(newFile)
+
+      return sha256.hexdigest()
+
+    def hashFileLOCAL(file):
+      BUF_SIZE = os.path.getsize(file)
+      sha256 = hashlib.sha256()
+      with open(file, 'rb') as f:
+        while True:
+          data = f.read(BUF_SIZE)
+
+          if not data:
+            break
+
+      sha256.update(data)
+
+      return sha256.hexdigest()
+
+    def install(URL, Destination, NewName, FileExt=""):
+      # Download and write to file
+      file_content = requests.get(URL)
+      open(Destination+'/'+NewName+FileExt,"wb").write(file_content.content)
+      if debug:
+        print("Downloaded file to: " + Destination)
+
+  class x32key:
+
+    @staticmethod
+    def x32_rawkey():
+      threshold = random.randint(7, 16)
+      buff = random.randint(500, 1000)
+      length = random.randint(8, 20)
+      iterable = 0
+      alphabet = string.digits + string.ascii_letters + string.digits
+      baseList = '.'.join(random.choice(alphabet)for i in range(buff)).split('.')
+
+      while iterable != threshold:
+        foo = baseList + list(random.choice(alphabet)for i in range(buff))
+        for item in range(length):
+          random.shuffle(foo)
+        iterable += 1
+
+        key = ''.join(random.choice(foo)for iterable in range(x32_keylength))
+
+      return str(key)
+
+  class x64key:
+
+    @staticmethod
+    def x64_rawkey():
+      threshold = random.randint(7, 16)
+      buff = random.randint(1000, 5000)
+      length = random.randint(16, 20)
+      iterable = 0
+      alphabet = string.ascii_letters + string.digits
+      baseList = '.'.join(random.choice(alphabet)for i in range(buff)).split('.')
+
+      while iterable != threshold:
+        foo = baseList + list(random.choice(alphabet)for i in range(buff))
+        for item in range(length):
+          random.shuffle(foo)
+        iterable += 1
+
+        key = ''.join(random.choice(foo)for iterable in range(x64_keylength))
+
+      return str(key)
+
+  class hexkey():
+
+    @staticmethod
+    def hex_rawkey():
+      threshold = random.randint(7, 16)
+      buff = random.randint(1000, 5000)
+      length = random.randint(16, 20)
+      lengthlist = [3, 4, 6]
+      if random.choice([True, False]):
+        for item in lengthlist:
+          int(random.choice(lengthlist) - 1)
       else:
         pass
-    for i in range(wordamount):
-      foo.append(random.choice(shortlist).decode('utf-8'))
-    for item in foo:
-      item = item.capitalize()
-      wordlist.append(item)
+      iterable = 0
+      alphabet = string.digits + string.ascii_uppercase + string.digits
+      baseList = '.'.join(random.choice(alphabet)for i in range(buff)).split('.')
 
-    key = ''.join(wordlist) + str(random.randint(175, 9999) - 75)
+      while iterable != threshold:
+        foo = baseList + list(random.choice(alphabet)for i in range(buff))
+        for item in range(length):
+          random.shuffle(foo)
+        iterable += 1
 
-    return str(key)
+        key = str('0' + 'x' + ''.join(
+          random.choice(foo)for iterable in range(random.choice(lengthlist))))
+
+      return str(key)
+
+  class wordkey():
+
+    @staticmethod
+    def word_rawkey():
+      wordamount = random.randint(1, 3)
+      foo = []
+      shortlist = []
+      wordlist = []
+      word_site = "https://www.mit.edu/~ecprice/wordlist.10000"
+      response = requests.get(word_site)
+      baselist = response.content.splitlines()
+
+      for word in baselist:
+        if len(word) <= random.randint(6, 12):
+          shortlist.append(word)
+        else:
+          pass
+      for i in range(wordamount):
+        foo.append(random.choice(shortlist).decode('utf-8'))
+      for item in foo:
+        item = item.capitalize()
+        wordlist.append(item)
+
+      key = ''.join(wordlist) + str(random.randint(175, 9999) - 75)
+
+      return str(key)
+
+  def split_key(key):
+    if not isinstance(key, str):
+      raise TypeError('`key` parameter must be a string object')
+
+    keylist = list(key)
+    returnkey = []
+    n = 4
+
+    for index in range(0, len(keylist), n):
+      returnkey.append(''.join(keylist[index:index + n]) + '.')
+    formattedKey = ''.join(returnkey)
+    return str(''.join(formattedKey[0:-1]))
+
+  def createkey(type):
+    if not isinstance(type, str):
+      raise TypeError(
+        '`type` parameter must be a string object and a valid option.')
+
+    if type == 'x32':
+      return _2PT.split_key(_2PT.x32key.x32_rawkey())
+    if type == 'x64':
+      return _2PT.split_key(_2PT.x64key.x64_rawkey())
+    if type == 'hex':
+      return _2PT.hexkey.hex_rawkey()
+    if type == 'wrd':
+      return _2PT.wordkey.word_rawkey()
+    else:
+      return 'ERROR: Invalid key type'
 
 
-def split_key(key):
-  if not isinstance(key, str):
-    raise TypeError('`key` parameter must be a string object')
+if __name__ == '__main__':
+  print('''
+---- 2PT Keys Console Tool ----
 
-  keylist = list(key)
-  returnkey = []
-  n = 4
+COMMANDS
 
-  for index in range(0, len(keylist), n):
-    returnkey.append(''.join(keylist[index:index + n]) + '.')
-  formattedKey = ''.join(returnkey)
-  return ''.join(formattedKey[0:-1])
+-new(type) : This command creates a new key of a certain type.
+-secure(key) : This will secure the given key and re-split it.
+-save_key(key) : Will save the given key in a txt file.
+-update : This function checks for an update and if applicable updates.
+-display_dir : This command simply prints the directory 2PT uses.
+-wipe_keys : Wipes the key save file.
 
+USAGE
+To pass a command type the desired command into the `> ` text field and press 'Enter'. (Examples shown below)
 
-def createkey(type):
-  if not isinstance(type, str):
-    raise TypeError('`type` parameter must be a string object and a valid option.')
+Command  |  Example
+new(type) : new x32
+secure(key) : secure 0x3H8I
+save_key(key) : save_key 0xR2D2
 
-  if type == 'x32':
-    return split_key(x32key.x32_rawkey())
-  if type == 'x64':
-    return split_key(x64key.x64_rawkey())
-  if type == 'hex':
-    return hexkey.hex_rawkey()
-  if type == 'wrd':
-    return wordkey.word_rawkey()
-  else:
-    return 'ERROR: Invalid key type'
+-------------------------------
+''')
 
+  userinput = input('> ')
+  inputlist = userinput.split(' ')
+  if inputlist[0] == 'new':
+    print(_2PT.createkey(inputlist[1]))
